831c9738b041ea5fad3e640776654a8d84134345
ElbieLi <zli3@seattleu.edu>
2024-08-22 06:26:32 +0000

    Added a POST method to create a new customer

--- src/main/java/com/webage/project/api/CustomerAPI.java
+++ src/main/java/com/webage/project/api/CustomerAPI.java
@@ -1,13 +1,18 @@
 package com.webage.project.api;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.webage.project.domain.Customer;
 import com.webage.project.repository.CustomerRepository;
@@ -35,5 +40,15 @@ public class CustomerAPI{
 		return repo.findById(id);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<?> addNewCustomer(@RequestBody Customer customer, UriComponentsBuilder uri){
+		if (customer.getId() != 0 || customer.getName() == null || customer.getEmail() == null || customer.getPassword() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		customer = repo.save(customer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(customer.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+		return response;
+	}
 }
