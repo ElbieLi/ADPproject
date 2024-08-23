131a4f69d5432bd94b86d4e5f5ea8faf60b96f32
ElbieLi <zli3@seattleu.edu>
2024-08-23 19:20:17 +0000

    Added unit test for the Get method that get a customer by id

--- src/test/java/com/webage/project/CustomerAPITests.java
+++ src/test/java/com/webage/project/CustomerAPITests.java
@@ -10,6 +10,8 @@ import com.webage.project.domain.Customer;
 import com.webage.project.repository.CustomerRepository;
 
 import java.util.Arrays;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -29,17 +31,32 @@ public class CustomerAPITests {
 
     @Test
     public void testGetAll(){
+        //mock the behavior of repo.findAll()
         Customer customer1 = new Customer(1, "Amy", "amy@adp.com", "pw123");
 
         Customer customer2 = new Customer(1, "John", "john@adp.com", "pw123");
 
+        //when repo.finall() is called, return a list of customers
         when(repo.findAll()).thenReturn(Arrays.asList(customer1, customer2));
 
+        //call the method under test
         Iterable<Customer> result = customerAPI.getAll();
 
+        //use assert to verify the result
         assertEquals(Arrays.asList(customer1, customer2), result);
     }
 
+    @Test
+    public void testGetCustomerById(){
+        Customer customer = new Customer(1, "Amy", "amy@adp.com", "pw123");
+
+        when(repo.findById((long) 1)).thenReturn(Optional.of(customer));
+
+        Optional<Customer> result = customerAPI.getCustomerById((long)1);
+
+        assertEquals(Optional.of(customer), result);
+
+    }
 
 
 }
