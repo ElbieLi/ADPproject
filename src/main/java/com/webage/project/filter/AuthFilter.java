b87f529568d4497096bdbc3d75c857a237d1999b
ElbieLi <zli3@seattleu.edu>
2024-08-23 09:07:32 +0000

    Added comments

--- src/main/java/com/webage/project/filter/AuthFilter.java
+++ src/main/java/com/webage/project/filter/AuthFilter.java
@@ -19,30 +19,25 @@ import com.webage.project.jwt.JWTHelper;
 @Order(1)
 public class AuthFilter implements Filter{
 
-	//public static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	private String auth_scope = "com.webage.auth.apis";
 	private String api_scope = "com.webage.data.apis";
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// get authorization header
+		//get authorization header
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		String uri = req.getRequestURI();
 		
 		
-		// for development purposes
-		// allow turning off auth checking with header tokencheck:false
+		//if token header is not null and false, skip authenticaiton check
 		String tokenheader = req.getHeader("tokencheck");
 		if( tokenheader != null && !tokenheader.equalsIgnoreCase("true") ) {
 			chain.doFilter(request, response);
 			return;		
 		}
 		
-		// auth checking will not apply to these cases
-		// token endpoint
-		// user register endpoint
-		// healthcheck endpoint on '/api/'
+		//the following cases will not has authentication check
 		if(   !uri.startsWith("/api/events") 
 	       && !uri.startsWith("/api/registrations")
 	       && !uri.equals("/api/customers")
@@ -50,7 +45,7 @@ public class AuthFilter implements Filter{
 			chain.doFilter(request, response);
 			return;			
 		}else{
-			// check JWT token
+			//check if the header of the client's request contains JWT token
 			String authheader = req.getHeader("authorization");
 			if(authheader != null && authheader.length() > 7 && authheader.startsWith("Bearer")) {
 				String jwt_token = authheader.substring(7, authheader.length());
@@ -63,7 +58,7 @@ public class AuthFilter implements Filter{
 				}
 			}
 		}		
-		// continue
+		//return error message if authentication failed
 		res.sendError(HttpServletResponse.SC_FORBIDDEN, "failed authentication");
 
 	}
